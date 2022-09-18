12e1a87e6579b7597b06805be7d40664fef59fae
Anatoly Semenov <anatolysrmenov@gmail.com>
2022-09-18 17:37:36 +0300

    Create index layout

---
+++ components/layout/layout-header.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="layout__header">
+		<div class="layout__container">
+			<div class="layout__header-main">
+				<nuxt-link class="layout__header-logo" to="/">
+					<logo-svg />
+				</nuxt-link>
+				<div class="layout__header-menu">
+					<nuxt-link
+						class="layout__header-link"
+						v-for="({ text, link }, index) in menu"
+						:key="index + link"
+						:to="link"
+					>
+						{{ text }}
+					</nuxt-link>
+				</div>
+			</div>
+			<div class="layout__header-auth">
+				<!--			Todo		-->
+				<ui-button>Регистрация</ui-button>
+				<ui-button>Войти</ui-button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+// Svg
+import logoSvg from "~/assets/img/logo.svg"
+
+export default Vue.extend({
+	name: "layout-header",
+	components: {
+		logoSvg
+	},
+	data() {
+		return {
+			menu: [
+				{
+					text: "Автомобили",
+					link: "/"
+				},
+				{
+					text: "Статьи",
+					link: "/"
+				}
+			]
+		}
+	}
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
 import Vue from "vue"
 
 export default Vue.extend({
-	name: "IndexPage"
+	name: "index-page",
+	layout: "index"
 })
 </script>
