627f4c4513fa3427ce0a047eacae5357017849eb
Anatoly Semenov <anatolysrmenov@gmail.com>
2022-10-23 14:05:36 +0300

    Create ui-modal component

--- plugins/global.ts
+++ plugins/global.ts
@@ -2,5 +2,7 @@ import Vue from "vue"
 
 // ui-components
 import UiButton from "~/components/ui-kit/ui-button.vue"
+import UiModal from "~/components/ui-kit/ui-modal.vue"
 
 Vue.component("ui-button", UiButton)
+Vue.component("ui-modal", UiModal)
